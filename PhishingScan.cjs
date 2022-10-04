cb946117c95db52b32fae8fabe7a7e043ea80b90
Manan Jethwani <50267986+MananJethwani@users.noreply.github.com>
2022-10-04 19:52:05 +0530

    Update PhishingScan.cjs

--- PhishingScan.cjs
+++ PhishingScan.cjs
@@ -15,7 +15,7 @@ function extractLinksFromFiles(Files) {
     let links = [];
     Files.forEach((file) => {
         const str = FS.readFileSync(file).toString();
-        links.push(...str.match(/([\w+]+\:\/\/)?([\w\d-]+\.)*[\w-]+[\.\:]\w+([\/\?\=\&\#\.]?[\w-]+)*\/?/gm))
+        links.push(...str.match(/(http|https)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/gm))
     });
     return links;
 }
